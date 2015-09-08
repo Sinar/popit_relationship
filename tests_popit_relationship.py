3c069655f55883263e6fe16331cbd48972ed6c16
lowks <kianseong@gmail.com>
2015-09-09 02:23:52 +0800

    Cleanup yesterday's tests and re-org code

--- tests_popit_relationship.py
+++ tests_popit_relationship.py
@@ -1,5 +1,4 @@
 import unittest
-import requests_mock
 from mock import Mock, patch, call
 from popit_to_neo4j import PopItToNeo
 
@@ -9,8 +8,10 @@ __author__ = 'lowks'
 class MyTestCase(unittest.TestCase):
 
     @patch("__builtin__.open")
+    @patch("py2neo.Graph.delete_all")
     @patch("yaml.load")
-    def setUp(self, mock_load_config, mock_open):
+    def setUp(self, mock_load_config, mock_graph_delete, mock_open):
+
         mock_open.return_value = "config.yaml"
         self.base_graph = dict(graph_db='graph_db',
                                refresh=True)
@@ -19,17 +20,21 @@ class MyTestCase(unittest.TestCase):
 
         def load_config(filename):
             if "config.yaml" in filename:
-                return self.base_auth
+                return self.base_graph
             elif "statuses.yaml" in filename:
                 return self.statuses
 
         mock_load_config.side_effect = load_config
+        self.popittoneo = PopItToNeo()
 
     @patch("__builtin__.open")
     @patch("py2neo.Graph.delete_all")
     @patch("yaml.load")
     def test_refresh_during_init(self, mock_load_config,
                                  mock_graph_delete, mock_open):
+
+        """Test to ensure that refresh is called during init"""
+
         mock_open.return_value = "config.yaml"
 
         def load_config(filename):
@@ -45,61 +50,33 @@ class MyTestCase(unittest.TestCase):
                      'post_processed'):
             self.assertEqual(getattr(popittoneo, attr), {})
 
-    @patch("__builtin__.open")
-    @patch("py2neo.Graph.delete_all")
-    @patch("yaml.load")
     @patch("popit_to_neo4j.requests.get")
-    def test_fetch_entity_status_400(self, mock_get, mock_load,
-                                     mock_delete_all, mock_open):
-            mock_open.return_value = "config.yaml"
+    def test_fetch_entity_with_status(self, mock_get):
 
-            def load_config(filename):
-                if "config.yaml" in filename:
-                    return self.base_graph
-                elif "statuses.yaml" in filename:
-                    return self.statuses
+        def return_json():
+            return "hulahoop"
 
-            mock_load.side_effect = load_config
-            mock_get.return_value.status_code = 400
-            popittoneo = PopItToNeo()
-            data = popittoneo.fetch_entity("http://www.google.com")
-            self.assertEqual(data, {})
-
-    @patch("__builtin__.open")
-    @patch("py2neo.Graph.delete_all")
-    @patch("yaml.load")
-    @patch("popit_to_neo4j.requests.get")
-    def test_fetch_entity_status_200(self, mock_get, mock_load,
-                                     mock_delete_all, mock_open):
-            mock_open.return_value = "config.yaml"
-
-            def load_config(filename):
-                if "config.yaml" in filename:
-                    return self.base_graph
-                elif "statuses.yaml" in filename:
-                    return self.statuses
-
-            mock_load.side_effect = load_config
-            mock_get.return_value.status_code = 200
-
-            def return_json():
-                return "hulahoop"
-
-            mock_get.return_value.json.side_effect = return_json
-            popittoneo = PopItToNeo()
-            data = popittoneo.fetch_entity("http://www.google.com")
-            self.assertEqual(data, "hulahoop")
+        for status, result in ((200, "hulahoop"), (400, {})):
+            mock_get.return_value.status_code = status
+            if status == 200:
+                mock_get.return_value.json.side_effect = return_json
+            else:
+                pass
+            data = self.popittoneo.fetch_entity("http://www.google.com")
+            self.assertEqual(data, result)
 
     @patch("time.mktime")
     def test_get_timestamp(self, mock_mktime):
+
+        """Test get_timestamp function"""
+
         from popit_to_neo4j import get_timestamp
         mock_mktime.return_value = "hulahoop"
-        for invalid_input in ('', 'hulahoop', '00000', '4-5-6'):
+        for invalid_input in ("", "hulahoop", "00000", "4-5-6"):
             self.assertIsNone(get_timestamp(invalid_input))
-        data = get_timestamp('2015-09-07')
+        data = get_timestamp("2015-09-07")
         self.assertTrue(mock_mktime.called)
-        self.assertEqual(data, 'hulahoop')
-
+        self.assertEqual(data, "hulahoop")
 
 if __name__ == '__main__':
     unittest.main()
