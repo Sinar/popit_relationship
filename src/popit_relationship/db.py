9845df43d8b51a5c27e3b30b68e58e28b82d0390
Jeffrey04 <mycyberpet@yahoo.com>
2020-07-12 10:11:53 +0800

    cleaning up unused function

--- src/popit_relationship/db.py
+++ src/popit_relationship/db.py
@@ -8,9 +8,3 @@ def driver_init():
         os.environ.get("NEO4J_URI", "bolt:localhost:7687"),
         auth=tuple(os.environ.get("NEO4J_AUTH", "neo4j/abc123").split("/")),
     )
-
-
-def init(session):
-    # do the actual delete here
-    # https://stackoverflow.com/questions/23310114/how-to-reset-clear-delete-neo4j-database
-    pass
